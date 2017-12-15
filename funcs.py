74957b6f5731d6e1c400ca1de0b8aee7f3e5ce1c
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2017-12-15 04:12:53 +0300

    Update funcs.py

--- funcs.py
+++ funcs.py
@@ -1,14 +1,9 @@
 
-
 import os
-
 global addressbartext
-
 HOME = os.environ['HOME']
-
 fm = {}
 pdict = {}
-
 def folder(u=HOME, h=False):
 	f = [f for f in os.listdir(u) if f.startswith('.') is h]
 	for isdir in f:
@@ -16,11 +11,9 @@ def folder(u=HOME, h=False):
 			fm[u+'/'+isdir] = {'dir':False, 'type':os.path.splitext(u+isdir)[1], 'label':isdir}
  		else: fm[u+'/'+isdir] = {'dir':True, 'type':'folder', 'label':isdir}
 	return fm
-
 def scanf(t='folder'):
 	keys = [keys for keys in fm if fm[keys]['type'] == t]
 	return keys
-
 def places():
 	with open(HOME+'/.config/user-dirs.dirs') as pla:
 		places = pla.read().splitlines()
@@ -28,13 +21,10 @@ def places():
 	for add in places:
 		pdict[add] = {'path':HOME+'/'+add, 'icon':'edit-copy'}
 	return pdict
-
 def oclick():
 	return 0
-
 def dclick():
 	return 0
-  
 print folder()
 print places()
 
