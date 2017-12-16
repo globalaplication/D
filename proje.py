ff6024719fd72368737cfd5e04246426b4db1266
Nuri Özdaş <32783197+globalaplication@users.noreply.github.com>
2017-12-17 00:58:40 +0300

    Update proje.py

--- proje.py
+++ proje.py
@@ -52,12 +52,6 @@ class IconViewWindow(Gtk.Window):
         hb = Gtk.HeaderBar()
         hb.set_show_close_button(True)
         self.window.set_titlebar(hb)
-
-        button = Gtk.Button()
-        icon = Gio.ThemedIcon(name="mail-send-receive-symbolic")
-        image = Gtk.Image.new_from_gicon(icon, Gtk.IconSize.BUTTON)
-        button.add(image)
-        hb.pack_end(button)
         
         box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL)
         Gtk.StyleContext.add_class(box.get_style_context(), "linked")
